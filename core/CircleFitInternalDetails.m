3f700b1a3194809842ff04444281635b62fd29c5
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2017-01-08 16:09:29 +0200

    Replace \textrm with \mathrm

--- core/CircleFitInternalDetails.m
+++ core/CircleFitInternalDetails.m
@@ -4,7 +4,7 @@ figure
 % Nyquist's circle visualization
 subplot(6,2,[1,3,5]);
 set(gca,'XAxisLocation','top')
-plot_FRF_Nyq([H_local,H_gen_local],'H^\textrm{local}',false,'.-');
+plot_FRF_Nyq([H_local,H_gen_local],'H^\mathrm{local}',false,'.-');
 
 % w_r point on the Nyquist diagram
 x_r = circ_prop.x_center+circ_prop.Radius*cos(theta_horizontal_r);
@@ -15,13 +15,13 @@ plot([circ_prop.x_center,x_r],[circ_prop.y_center,y_r],'k');
 % FRF magnitude
 ax_mag_h=subplot(6,2,[2,4,6,8]);
 ax_phase_h=subplot(6,2,[10,12]);
-plot_FRF_mag_phase(f_local,[H_local,H_gen_local],false,ax_mag_h,ax_phase_h,'','H^\textrm{local}');
+plot_FRF_mag_phase(f_local,[H_local,H_gen_local],false,ax_mag_h,ax_phase_h,'','H^\mathrm{local}');
 
 legend('Measured FRF','Generated FRF');
 
 subplot(6,2,[7,9,11]);
 surf(f_before_mat,f_after_mat,eta_r_mat);
 axis tight
-xlabel('$f_{\textrm{before}}$, (Hz)','interpreter', 'latex');
-ylabel('$f_{\textrm{after}}$, (Hz)','interpreter', 'latex');
+xlabel('$f_{\mathrm{before}}$, (Hz)','interpreter', 'latex');
+ylabel('$f_{\mathrm{after}}$, (Hz)','interpreter', 'latex');
 zlabel('$\eta_r$','interpreter', 'latex');
